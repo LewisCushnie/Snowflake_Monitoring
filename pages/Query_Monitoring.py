a37984cd7016875a8d746ca582d9d914950478ae
dougaltoms <dougal.toms@gmail.com>
2022-12-07 17:11:01 +0000

    Update Query_Monitoring.py

--- pages/Query_Monitoring.py
+++ pages/Query_Monitoring.py
@@ -19,7 +19,7 @@ def main():
     df = sf.sql_to_dataframe(query)
     df = df.transpose()
 
-    current_user = df.loc['CURRENT_USER'][0] 
+    current_user = df.loc['CURRENT_USER']
     st.sidebar.write(f'Hello, {current_user}')
 
     st.sidebar.dataframe(df)
