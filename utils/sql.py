0faa5dea6b07b3de53574a0d3c25377db4f978a3
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-07 11:00:58 +0000

    A

--- utils/sql.py
+++ utils/sql.py
@@ -1,3 +1,5 @@
+#-------------------- SIDEBAR ------------------------
+
 TEST_QUERY = '''select current_database() 
 ,current_schema()
 ,current_role()
@@ -7,12 +9,15 @@ TEST_QUERY = '''select current_database()
 ,current_region()
 ,current_time();'''
 
+#-------------- RESOURCE MONITORING ------------------
+
 STREAMLIT_CREDITS_USED = '''select
 sum(credits_used_cloud_services)
 from query_history
 where query_tag = 'StreamlitQuery';'''
 
-SNOWFLAKE_SESSION_VARIABLES = '''select current_database() 
+SNOWFLAKE_SESSION_VARIABLES = '''select 
+current_database() 
 ,current_schema()
 ,current_role()
 ,current_session()
@@ -21,20 +26,20 @@ SNOWFLAKE_SESSION_VARIABLES = '''select current_database()
 ,current_region()
 ,current_time();'''
 
-USER_LIST = ''' SELECT distinct(user_name)
-FROM SNOWFLAKE.ACCOUNT_USAGE.QUERY_HISTORY'''
+METERING_HISTORY = ''' select 
+name
+,credits_used 
+from metering_history;'''
 
-USER_QUERY_HISTORY =  '''SELECT user_name
-    , avg(percentage_scanned_from_cache)
-    , avg(partitions_scanned)
-    , avg(partitions_total)
-    , avg(execution_time)
-    , avg(query_load_percent)
-    , avg(credits_used_cloud_services)
-    FROM SNOWFLAKE.ACCOUNT_USAGE.QUERY_HISTORY 
-    GROUP BY user_name
-    ORDER BY avg(partitions_total) DESC;
-    '''
+METERING_TOP_10 = '''select top 10 
+name 
+,sum(credits_used) 
+from metering_history 
+group by name; '''
+
+#-------------- RBAC SUMMARY ------------------
+
+#-------------- QUERY MONITORING ------------------
 
 if __name__ == "__main__":
     pass
