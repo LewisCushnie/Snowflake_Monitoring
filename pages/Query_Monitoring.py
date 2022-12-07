95843840f6cbccc51beb86cfb0284e2b88bad3b6
dougaltoms <dougal.toms@gmail.com>
2022-12-07 12:39:26 +0000

    Update Query_Monitoring.py

--- pages/Query_Monitoring.py
+++ pages/Query_Monitoring.py
@@ -56,7 +56,8 @@ def main():
 
     query = sql.USER_QUERY_HISTORY
     df = sf.sql_to_dataframe(query)
-    df = df.set_index('USERNAME')
+    st.dataframe(df)
+    df = df.set_index('USER_NAME')
 
     selected_username = st.multiselect('Select a user', clean_users)
     df = df.loc[selected_username]                    
