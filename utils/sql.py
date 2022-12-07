7e59d7eb3de9b3391c4d7f5a14ad3f3a6508293e
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-07 10:55:01 +0000

    a

--- utils/sql.py
+++ utils/sql.py
@@ -7,5 +7,19 @@ TEST_QUERY = '''select current_database()
 ,current_region()
 ,current_time();'''
 
+STREAMLIT_CREDITS_USED = '''select
+sum(credits_used_cloud_services)
+from query_history
+where query_tag = 'StreamlitQuery';'''
+
+SNOWFLAKE_SESSION_VARIABLES = '''select current_database() 
+,current_schema()
+,current_role()
+,current_session()
+,current_user()
+,current_warehouse()
+,current_region()
+,current_time();'''
+
 if __name__ == "__main__":
     pass
