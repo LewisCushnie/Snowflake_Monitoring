d16a7a9a41773cbff177414fd41c967a67568c07
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-07 13:54:25 +0000

    a

--- utils/sql.py
+++ utils/sql.py
@@ -34,7 +34,7 @@ name
 ,sum(credits_used)
 from metering_history 
 group by name
-order by sum(credits_used); '''
+order by sum(credits_used) asc; '''
 
 #-------------- RBAC SUMMARY ------------------
 ALL_RBAC_ROLES = '''
