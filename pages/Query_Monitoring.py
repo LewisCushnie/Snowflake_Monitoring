cca13dcf9238ac99643c6b5e0180ece05e1741f1
dougaltoms <dougal.toms@gmail.com>
2022-12-07 14:49:34 +0000

    Update Query_Monitoring.py

--- pages/Query_Monitoring.py
+++ pages/Query_Monitoring.py
@@ -31,8 +31,8 @@ def main():
 
     #------------------------------- SIDEBAR ----------------------------------- 
 
-    # with open("utils/style.css") as f:
-    #     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+    with open("utils/style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     st.title('Query Monitoring')
 
