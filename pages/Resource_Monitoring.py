9b0c87cab18743d6b11fbc69bb204947427e31a2
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-07 14:33:44 +0000

    bug

--- pages/Resource_Monitoring.py
+++ pages/Resource_Monitoring.py
@@ -75,8 +75,8 @@ def main():
         st.dataframe(METERING_TOP_10_df)
 
     most_used_wh = METERING_TOP_10_df['SUM(CREDITS_USED)'].idxmax()
-    # st.write(WH_USAGE_LAST_7_DAYS_df['NAME'].iloc[most_used_wh])
-    st.write(WH_USAGE_LAST_7_DAYS_df['SUM(CREDITS_USED)'].iloc[most_used_wh])
+    st.write(METERING_TOP_10_df['NAME'].iloc[most_used_wh])
+    st.write(METERING_TOP_10_df['SUM(CREDITS_USED)'].iloc[most_used_wh])
 
     st.header('Warehouse usage comparison chart')
     METERING_TOP_10_df = METERING_TOP_10_df.set_index('NAME')
