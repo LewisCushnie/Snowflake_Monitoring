2874b042a7008025803191762169d3fcec631333
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-07 15:13:15 +0000

    A

--- utils/sql.py
+++ utils/sql.py
@@ -6,14 +6,14 @@ from query_history
 where query_tag = 'StreamlitQuery';'''
 
 SNOWFLAKE_ACCOUNT_PARAMS = '''
-select current_database() as Database
-,current_schema() as Schema
-,current_role() as Current role
-,current_session() as Session ID
-,current_user() as Current user
-,current_warehouse() as Warehouse
-,current_region() as Acccount region
-,current_time() as Region time
+select current_database() as DATABASE
+,current_schema() as SHEMA
+,current_role() as CURRENT_ROLE
+,current_session() as SESSION_ID
+,current_user() as CURRENT_USER
+,current_warehouse() as WAREHOUSE
+,current_region() as ACCOUNT_REGION
+,current_time() as REGION_TIME
 ;'''
 
 METERING_HISTORY = ''' 
