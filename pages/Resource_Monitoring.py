d086e9c878e97a86ef93a2597d4e1aaac5bf3a43
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-08 16:51:10 +0000

    a

--- pages/Resource_Monitoring.py
+++ pages/Resource_Monitoring.py
@@ -40,7 +40,7 @@ def main():
     STREAMLIT_CREDITS_USED_df = sf.sql_to_dataframe(query)
     metric=round(STREAMLIT_CREDITS_USED_df['CREDITS_USED_STREAMLIT'].iloc[0],5)
     remaining=round(100-metric,3)
-    st.sidebar.metric(label='Credits used by Streamlit', value =metric, delta=f'{remaining} remaining')
+    st.sidebar.metric(label='**Credits used by Streamlit:**', value =metric, delta=f'{remaining} remaining')
 
     line = '---'
     st.sidebar.markdown(line)
