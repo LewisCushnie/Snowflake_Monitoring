1e2736dc34759a6c8acc53d0518e8f921bd94565
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-12 12:02:26 +0000

    a

--- pages/Query_Monitoring.py
+++ pages/Query_Monitoring.py
@@ -113,7 +113,7 @@ def main():
 
     if DOMAIN:
         df = sf.sql_to_dataframe(DOMAIN_QUERY_USAGE)
-        st.dataframe(df, use_container_width=True)
+        st.dataframe(df, width=1000)
         st.bar_chart(df, x='Schema', y=['Total Compute Credits Used', 'Total Credits Used'])
 
 
