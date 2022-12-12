85f1b28e1206a6329001d26445ce4ba3dd3840e3
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-12 11:49:02 +0000

    a

--- pages/RBAC_Summary.py
+++ pages/RBAC_Summary.py
@@ -31,7 +31,7 @@ def main():
 
         df = sf.sql_to_dataframe(RBAC)
         df = df[['name', 'assigned_to_users', 'granted_to_roles', 'granted_roles']]
-        df.set_index('name')
+        df = df.set_index('name')
 
 
         if selection=='PROD':
