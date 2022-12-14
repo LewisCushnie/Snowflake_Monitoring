cd93395fc68e4939cedad672914534e4b66c8575
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-14 11:34:36 +0000

    Add user form

--- pages/Query_Monitoring.py
+++ pages/Query_Monitoring.py
@@ -93,6 +93,76 @@ def main():
                             help='Click to download user query history as a csv')
 
     #==========================#
+    # USER IN DETAIL #
+    #==========================#
+    line = '---'
+    st.markdown(line)
+    st.header('Get Detailed Performance Info by User')
+
+    with st.form('user_form'):
+        user = st.multiselect('Select a user', clean_users)
+        submitted = st.form_submit_button('Submit')
+
+        if submitted:
+
+        LOGIN = f'''
+        select last_success_login
+        from users
+        where name = {user};
+        '''
+
+        CREDITS_BY_USER = f'''
+        WITH USER_HOUR_EXECUTION_CTE AS (
+            SELECT  USER_NAME
+            ,WAREHOUSE_NAME
+            ,DATE_TRUNC('hour',START_TIME) as START_TIME_HOUR
+            ,SUM(EXECUTION_TIME)  as USER_HOUR_EXECUTION_TIME
+            FROM "SNOWFLAKE"."ACCOUNT_USAGE"."QUERY_HISTORY" 
+            WHERE WAREHOUSE_NAME IS NOT NULL
+            AND EXECUTION_TIME > 0
+        
+        --Change the below filter if you want to look at a longer range than the last 1 month 
+            AND START_TIME > DATEADD(Month,-1,CURRENT_TIMESTAMP())
+            group by 1,2,3
+            )
+        , HOUR_EXECUTION_CTE AS (
+            SELECT  START_TIME_HOUR
+            ,WAREHOUSE_NAME
+            ,SUM(USER_HOUR_EXECUTION_TIME) AS HOUR_EXECUTION_TIME
+            FROM USER_HOUR_EXECUTION_CTE
+            group by 1,2
+        )
+        , APPROXIMATE_CREDITS AS (
+            SELECT 
+            A.USER_NAME
+            ,C.WAREHOUSE_NAME
+            ,(A.USER_HOUR_EXECUTION_TIME/B.HOUR_EXECUTION_TIME)*C.CREDITS_USED AS APPROXIMATE_CREDITS_USED
+
+            FROM USER_HOUR_EXECUTION_CTE A
+            JOIN HOUR_EXECUTION_CTE B  ON A.START_TIME_HOUR = B.START_TIME_HOUR and B.WAREHOUSE_NAME = A.WAREHOUSE_NAME
+            JOIN "SNOWFLAKE"."ACCOUNT_USAGE"."WAREHOUSE_METERING_HISTORY" C ON C.WAREHOUSE_NAME = A.WAREHOUSE_NAME AND C.START_TIME = A.START_TIME_HOUR
+        )
+
+        SELECT 
+        USER_NAME
+        ,WAREHOUSE_NAME
+        ,SUM(APPROXIMATE_CREDITS_USED) AS APPROXIMATE_CREDITS_USED
+        FROM APPROXIMATE_CREDITS
+        WHERE USER_NAME = {user}
+        GROUP BY 1,2
+        ORDER BY 3 DESC
+        ;
+
+        '''
+
+        df = sf.sql_to_dataframe(LOGIN)
+        st.write(df)
+
+        df = sf.sql_to_dataframe(CREDITS_BY_USER)
+        st.dataframe(df)
+
+
+    #==========================#
     # DOMAIN QUERY PERFORMANCE #
     #==========================#
     line = '---'
