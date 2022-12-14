76991d919277438018a055904f3442817dbb7087
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-14 14:16:16 +0000

    a

--- utils/sql.py
+++ utils/sql.py
@@ -115,7 +115,7 @@ FROM SNOWFLAKE.ACCOUNT_USAGE.QUERY_HISTORY
 
 USER_QUERY_HISTORY = '''
 SELECT user_name as "Username"
-    , ROUND(avg(percentage_scanned_from_cache)*100,2)::float as "Avg Scanned from Cache (%)"
+    , avg(percentage_scanned_from_cache)*100::float as "Avg Scanned from Cache (%)"
     , avg(partitions_scanned)::float as "Avg Partitions Scanned"
     , avg(partitions_total)::float as "Avg Partitions Used"
     , avg(execution_time)::float as "Avg Execution Time"
