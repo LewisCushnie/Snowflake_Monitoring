33613b826260ebf8e656cf5886b590b8122f77c1
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-15 11:47:25 +0000

    a

--- pages/Best_Practice_Monitoring.py
+++ pages/Best_Practice_Monitoring.py
@@ -74,7 +74,7 @@ def main():
     st.dataframe(EMPTY_TABLES_AND_VIEWS_IN_ACCOUNT_df)
 
     st.header('Unused tables in account')
-    days = 28
+    days = 3
     query = sql.UNUSED_TABLES_AND_VIEWS_IN_ACCOUNT(days)
     UNUSED_TABLES_AND_VIEWS_IN_ACCOUNT_df = sf.sql_to_dataframe(query)
     st.dataframe(UNUSED_TABLES_AND_VIEWS_IN_ACCOUNT_df)
