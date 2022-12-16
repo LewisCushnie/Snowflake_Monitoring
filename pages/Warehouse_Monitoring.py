bcd1ee3e06ffeeb563dc19c89c8623cd01e864bf
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-16 15:51:09 +0000

    a

--- pages/Warehouse_Monitoring.py
+++ pages/Warehouse_Monitoring.py
@@ -77,11 +77,12 @@ def main():
 
     query = sql.WH_CREDIT_BREAKDOWN
     WH_CREDIT_BREAKDOWN_df = sf.sql_to_dataframe(query)
+    #WH_CREDIT_BREAKDOWN_df = WH_CREDIT_BREAKDOWN_df.set_index('WH_NAME')
     WH_CREDIT_BREAKDOWN_df['TOTAL_CREDITS'] = WH_CREDIT_BREAKDOWN_df['TOTAL_CREDITS'].astype(float)
 
     # Top 5 most used warehouses
     five_most_used_df = WH_CREDIT_BREAKDOWN_df['TOTAL_CREDITS'].nlargest(5)
-    five_most_used_wh_list = WH_CREDIT_BREAKDOWN_df['WH_NAME'].iloc[five_most_used_df.index].tolist()
+    five_most_used_wh_list = WH_CREDIT_BREAKDOWN_df['WH_NAME'].iloc[five_most_used_df['WH_NAME']].tolist()
 
     st.write(five_most_used_wh_list)
     st.write(WH_CREDIT_BREAKDOWN_df)
@@ -99,7 +100,7 @@ def main():
     if selection == 'Warehouse comparison':
         # Multiselect list
         wh_selected = st.multiselect("Pick Warehouse (5 most used warehouses selected by default):",\
-        list(WH_CREDIT_BREAKDOWN_df['WH_NAME']), five_most_used_wh_list)
+        list(WH_CREDIT_BREAKDOWN_df.index), five_most_used_wh_list)
         WH_CREDIT_df = WH_CREDIT_BREAKDOWN_df.loc[wh_selected]
 
         # Create altair chart
