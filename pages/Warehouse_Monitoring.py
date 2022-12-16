87e95f2d1d5382b71bb302005a5c01fccb9c8a73
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-16 12:50:51 +0000

    a

--- pages/Warehouse_Monitoring.py
+++ pages/Warehouse_Monitoring.py
@@ -142,6 +142,10 @@ def main():
     st.markdown(line)
     st.header('Warehouse credit usage breakdown')
 
+
+    METERING_TOP_10_df = METERING_TOP_10_df.set_index('NAME')
+    METERING_TOP_10_df['CREDITS_USED'] = METERING_TOP_10_df['CREDITS_USED'].astype(float)
+    
     query = sql.WH_CREDIT_BREAKDOWN
     WH_CREDIT_BREAKDOWN_df = sf.sql_to_dataframe(query)
 
@@ -152,7 +156,8 @@ def main():
 
     if selection == 'A':
         # Multiselect list
-        wh_selected = st.multiselect("Pick Warehouse (5 most used warehouses selected by default):", list(METERING_TOP_10_df.index), five_most_used_wh_list)
+        wh_selected = st.multiselect("Pick Warehouse (5 most used warehouses selected by default):",\
+        list(METERING_TOP_10_df.index), five_most_used_wh_list)
         WH_CREDIT_BREAKDOWN_df = METERING_TOP_10_df.loc[wh_selected]
     elif selection == 'n most used warehouses':
         # Top n highest total credit usage warehouses
