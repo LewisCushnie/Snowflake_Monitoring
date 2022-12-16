e5db32a238683beb3c6eef4b8fb754d6264eb72e
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-16 11:03:02 +0000

    A

--- pages/Task_Monitoring.py
+++ pages/Task_Monitoring.py
@@ -37,6 +37,9 @@ def main():
     query = sql.TASK_HISTORY
     SHOW_TASKS_df = sf.sql_to_dataframe(query)
 
+    st.display(SHOW_TASKS_df)
+    st.display(SHOW_TASKS_df.melt())
+
     st.bar_chart(SHOW_TASKS_df, x= 'NAME', y= ['COUNT_SUCCEEDED', 'COUNT_FAILED'])
 
     # chart = alt.Chart(SHOW_TASKS_df).mark_bar().encode(
@@ -44,14 +47,29 @@ def main():
     # y=alt.Y('site:N', sort='-x')
     # )
 
-    a = alt.Chart(SHOW_TASKS_df).mark_bar().encode(
-    x='NAME', y='COUNT_SUCCEEDED')
+    # .transform_fold(
+    # ['AAPL', 'AMZN', 'GOOG'],
+    # as_=['company', 'price']
 
-    b = alt.Chart(SHOW_TASKS_df).mark_bar().encode(
-    x='NAME', y='COUNT_FAILED')
+    test = alt.Chart(SHOW_TASKS_df).transform_fold(
+    ['COUNT_SUCCEEDED', 'COUNT_FAILED'],
+    as_=['STATUS', 'COUNT']
+    ).mark_bar().encode(
+    x= 'NAME',
+    y= 'COUNT'
+    color= 'STATUS'
+    )
 
-    c = alt.layer(a, b)
-    st.altair_chart(c, use_container_width=True, theme= 'streamlit')
+    st.altair_chart(test, theme= 'streamlit')
+
+    # a = alt.Chart(SHOW_TASKS_df).mark_bar().encode(
+    # x='NAME', y='COUNT_SUCCEEDED')
+
+    # b = alt.Chart(SHOW_TASKS_df).mark_bar().encode(
+    # x='NAME', y='COUNT_FAILED')
+
+    # c = alt.layer(a, b)
+    # st.altair_chart(c, use_container_width=True, theme= 'streamlit')
     
     raw_data = st.checkbox('Show raw task history data:')
     if raw_data:
