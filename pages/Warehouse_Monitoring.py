06cf7712b318c97cbc1103eb1bb5a693fb08482b
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-16 13:03:45 +0000

    a

--- pages/Warehouse_Monitoring.py
+++ pages/Warehouse_Monitoring.py
@@ -182,10 +182,10 @@ def main():
         # Multiselect list
         wh_selected = st.multiselect("Pick Warehouse (5 most used warehouses selected by default):",\
         list(METERING_TOP_10_df.index), five_most_used_wh_list)
-        wh_to_show_df = METERING_TOP_10_df.loc[wh_selected]
+        WH_CREDIT_df = METERING_TOP_10_df.loc[wh_selected]
 
         # Create altair chart
-        chart = alt.Chart(wh_to_show_df.reset_index()).mark_bar().encode(
+        chart = alt.Chart(WH_CREDIT_df.reset_index()).mark_bar().encode(
         x= alt.X('NAME:N', sort= '-y'),
         y= alt.Y('CREDITS_USED:Q')
         )
