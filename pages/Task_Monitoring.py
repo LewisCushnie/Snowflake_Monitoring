8dd6c86b00c7e469d8d6506aa1f4d8de35b3df07
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-16 10:36:00 +0000

    A

--- pages/Task_Monitoring.py
+++ pages/Task_Monitoring.py
@@ -50,6 +50,22 @@ def main():
     y=alt.Y('RUNS')
     )
 
+    df = pd.DataFrame({
+        'name': ['brian', 'dominik', 'patricia'],
+        'age': [20, 30, 40],
+        'salary': [100, 200, 300]
+    })
+
+    a = alt.Chart(df).mark_area(opacity=1).encode(
+        x='name', y='age')
+
+    b = alt.Chart(df).mark_area(opacity=0.6).encode(
+        x='name', y='salary')
+
+    c = alt.layer(a, b)
+
+    st.altair_chart(c, use_container_width=True)
+
     # df = pd.DataFrame([['Action', 5, 'F'], 
     #                 ['Crime', 10, 'F'], 
     #                 ['Action', 3, 'M'], 
