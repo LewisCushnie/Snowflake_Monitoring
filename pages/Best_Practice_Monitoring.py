f47414e7446d9f42c421eb0ebe90e8ef4816b5dd
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-17 16:56:07 +0000

    a

--- pages/Best_Practice_Monitoring.py
+++ pages/Best_Practice_Monitoring.py
@@ -58,34 +58,8 @@ def main():
 
     # selection_rows = EMPTY_TABLES_AND_VIEWS_IN_ACCOUNT_df['TABLE_SCHEMA'].str.contains(selections)
     # filtered_df = EMPTY_TABLES_AND_VIEWS_IN_ACCOUNT_df.loc[selection_rows]
-
-    def filter_df_by_business_domain(df):
-
-        if 'boobs' in df:
-            st.write('YAY')
-        else:
-            raise Exception('''The 'TABLE_SCHEMA' column does not exist in this dataframe, 
-            cannot use filter_df_by_business_domain function''')
-
-        query = sql.BUSINESS_DOMAINS
-        BUSINESS_DOMAINS_df = sf.sql_to_dataframe(query)
-
-        select_all = st.checkbox('Select all business domains:')
-
-        if select_all:
-            selections = fun.list_to_OR_string(BUSINESS_DOMAINS_df['DOMAIN_NAME'])
-        else:
-            # Multiselect list
-            multi_selections = st.multiselect("Select domain(s) to filter by:",\
-            list(BUSINESS_DOMAINS_df['DOMAIN_NAME']), list(BUSINESS_DOMAINS_df['DOMAIN_NAME']))
-            selections = fun.list_to_OR_string(multi_selections)
-
-        selection_rows = df['TABLE_SCHEMA'].str.contains(selections)
-        filtered_df = df.loc[selection_rows]
-
-        return filtered_df
     
-    filtered_df = filter_df_by_business_domain(EMPTY_TABLES_AND_VIEWS_IN_ACCOUNT_df)
+    filtered_df = fun.filter_df_by_business_domain(EMPTY_TABLES_AND_VIEWS_IN_ACCOUNT_df)
 
     # Colour formatting
     filtered_df = filtered_df.style.applymap(sty.make_red,
--- utils/functions.py
+++ utils/functions.py
@@ -19,3 +19,33 @@ def list_to_OR_string(input_list):
                 i += 1
 
     return OR_string
+
+
+def filter_df_by_business_domain(df):
+
+    # check that the input df contains a 'TABLE SCHEMA' column
+    if 'TABLE_SCHEMA' in df:
+        st.write('YAY')
+    else:
+        raise Exception('''The 'TABLE_SCHEMA' column does not exist in this dataframe, 
+        cannot use the filter_df_by_business_domain function''')
+
+    # get the business domains from the ADMIN_DB
+    query = sql.BUSINESS_DOMAINS
+    BUSINESS_DOMAINS_df = sf.sql_to_dataframe(query)
+
+    select_all = st.checkbox('Select all business domains:')
+
+    if select_all:
+        selections = fun.list_to_OR_string(BUSINESS_DOMAINS_df['DOMAIN_NAME'])
+
+    else:
+        # Multiselect list
+        multi_selections = st.multiselect("Select domain(s) to filter by:",\
+        list(BUSINESS_DOMAINS_df['DOMAIN_NAME']), list(BUSINESS_DOMAINS_df['DOMAIN_NAME']))
+        selections = fun.list_to_OR_string(multi_selections)
+
+    selection_rows = df['TABLE_SCHEMA'].str.contains(selections)
+    filtered_df = df.loc[selection_rows]
+
+    return filtered_df
