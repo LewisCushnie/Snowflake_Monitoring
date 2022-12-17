5bb61d093644d169dbe770430e87cc6c40116083
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-17 15:02:08 +0000

    a

--- utils/sql.py
+++ utils/sql.py
@@ -20,7 +20,7 @@ def UNUSED_TABLES_AND_VIEWS_IN_ACCOUNT(days):
         ,last_altered
     from snowflake.account_usage.tables
     where last_altered < dateadd( 'DAY', -{days}, current_timestamp() ) 
-    order by days_unused;
+    order by days_unused desc;
     '''
     return query
 
