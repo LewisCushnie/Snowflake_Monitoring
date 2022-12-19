eef175dd39499bc7af42d7345dfc35a1fcd4b12a
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2022-12-19 11:15:33 +0000

    a

--- pages/Best_Practice_Monitoring.py
+++ pages/Best_Practice_Monitoring.py
@@ -148,7 +148,7 @@ def main():
     st.info('This section provides analysis on the account\'s warehouses')  
 
    # ----------------- WAREHOUSES WITHOUT MONITOR ----------------------
-    st.header('(1) Warehouses that do not have a resource monitor')
+    st.subheader('Warehouses that do not have a resource monitor')
 
     query = sql.WAREHOUSE_DETAILS
     WAREHOUSE_DETAILS_df = sf.sql_to_dataframe(query)
@@ -176,7 +176,7 @@ def main():
         )
 
    # ----------------- WAREHOUSE UTILIZATION SUMMARY ----------------------
-    st.subheader('(2.0) Warehouse utilisation - Summary')
+    st.subheader('Warehouse utilisation - Summary')
 
     query = sql.WAREHOUSE_UTILIZATION_LAST_N_DAYS
     WAREHOUSE_UTILIZATION_LAST_N_DAYS_df = sf.sql_to_dataframe(query)
