cb17f2f85029aed7c3968d804cb76cf5bf36dba8
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-22 12:34:22 +0000

    Logo

--- Home.py
+++ Home.py
@@ -19,9 +19,8 @@ def main():
     # Apply formatting to page
     with open("utils/style.css") as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-    image = Image.open('https://www.beazley.com/themes/custom/beazley_default/logo.svg')
-    st.image(image)
+        
+    st.image('https://www.beazley.com/themes/custom/beazley_default/logo.svg')
 
     #======================================================#
     # SIDEBAR - SNOWFLAKE ACCOUNT PARAMETERS
