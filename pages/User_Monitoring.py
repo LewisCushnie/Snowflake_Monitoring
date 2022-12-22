f13068b713d1c10dd3fd5e4fc35d6571cac60720
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-22 12:09:11 +0000

    a

--- pages/User_Monitoring.py
+++ pages/User_Monitoring.py
@@ -50,6 +50,7 @@ def main():
             CREDITS_BY_USER_df = sf.sql_to_dataframe(CREDITS_BY_USER)
 
             try:
+                st.write(LOGIN_df['LAST_LOGIN'][0])
                 if len(LOGIN_df['LAST_LOGIN'][0]) > 0:
                     last_login = LOGIN_df['LAST_LOGIN'][0]
                     name = LOGIN_df['NAME'][0]
