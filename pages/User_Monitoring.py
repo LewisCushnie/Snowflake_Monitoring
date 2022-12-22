3ad9a0fae37b19389c9baa0c8ae88cd129ecf823
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-22 16:51:53 +0000

    a

--- pages/User_Monitoring.py
+++ pages/User_Monitoring.py
@@ -62,7 +62,7 @@ def main():
                 last_login = LOGIN_df['LAST_LOGIN'][0]
                 role = LOGIN_df['DEFAULT_ROLE'][0]
                 name = LOGIN_df['NAME'][0]
-                st.wrtie(f'{user} ({role})')
+                st.write(f'{user} ({role})')
                 st.info(body=f"Last active: {last_login} day(s) ago", icon='ℹ️')
 
         
