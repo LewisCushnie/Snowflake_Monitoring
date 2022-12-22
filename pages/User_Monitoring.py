c8d745bed3db550263aee17128419f63a1cd87a1
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-12-22 12:08:42 +0000

    a

--- pages/User_Monitoring.py
+++ pages/User_Monitoring.py
@@ -50,9 +50,12 @@ def main():
             CREDITS_BY_USER_df = sf.sql_to_dataframe(CREDITS_BY_USER)
 
             try:
-                last_login = LOGIN_df['LAST_LOGIN'][0]
-                name = LOGIN_df['NAME'][0]
-                st.info(body=f"{name} last active: {last_login} day(s) ago", icon='ℹ️')
+                if len(LOGIN_df['LAST_LOGIN'][0]) > 0:
+                    last_login = LOGIN_df['LAST_LOGIN'][0]
+                    name = LOGIN_df['NAME'][0]
+                    st.info(body=f"{name} last active: {last_login} day(s) ago", icon='ℹ️')
+                else:
+                    pass
 
             except Exception as e:
                 st.write(e)
