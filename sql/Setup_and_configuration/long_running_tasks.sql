902dfdf61b252973342a1a13781476d2cc53dcfe
Lewis Cushnie <106535310+LewisCushnie@users.noreply.github.com>
2023-01-17 16:19:05 +0000

    more queries

---
+++ sql/Setup_and_configuration/long_running_tasks.sql
@@ -0,0 +1,16 @@
+-- Description:
+-- Returns an ordered list of the longest running tasks
+
+-- How to Interpret Results:
+-- revisit task execution frequency or the task code for optimization
+
+-- Primary Schema:
+-- Account_Usage
+
+select DATEDIFF(seconds, QUERY_START_TIME,COMPLETED_TIME) as DURATION_SECONDS
+                ,*
+from snowflake.account_usage.task_history
+WHERE STATE = 'SUCCEEDED'
+and query_start_time >= DATEADD (day, -7, CURRENT_TIMESTAMP())
+order by DURATION_SECONDS desc
+  ;
---
+++ sql/Setup_and_configuration/users_never_logged_in.sql
@@ -0,0 +1,13 @@
+-- Description:
+-- Users that have never logged in to Snowflake
+
+-- How to Interpret Results:
+-- Should these users be removed or more formally onboarded?
+
+-- Primary Schema:
+-- Account_Usage
+
+SELECT 
+	*
+FROM SNOWFLAKE.ACCOUNT_USAGE.USERS 
+WHERE LAST_SUCCESS_LOGIN IS NULL;
